f3846b0d2f8afb5f0f8e256ee14101f4d24b3494
Chandru <ck6582293@gmail.com>
2026-01-03 11:42:35 +0530

    render issue fix

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "http://127.0.0.1:5173"
-  ],
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
